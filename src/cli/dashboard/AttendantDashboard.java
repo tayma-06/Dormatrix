6ceb8d3fa81a69b116e056be0d2bdad10b5c522a
sApphire2004-alt <prochetasilvie.p423@gmail.com>
2025-12-08 23:22:47 +0600

    Modified Some Files

--- src/cli/dashboard/AttendantDashboard.java
+++ src/cli/dashboard/AttendantDashboard.java
@@ -1,4 +1,7 @@
 package cli.dashboard;
 
 public class AttendantDashboard {
+    public static void main(String[] args) {
+
+    }
 }
--- src/models/users/Student.java
+++ src/models/users/Student.java
@@ -1,4 +1,30 @@
 package models.users;
 
-public class Student {
+public class Student extends User{
+    private String department;
+    private String batch;
+    private String roomNumber; // initially "UNASSIGNED"
+    private String email;
+
+    public Student(String id, String name, String passwordHash,
+            String department, String email
+    )
+    {
+        super(id, name, "STUDENT", passwordHash);
+        this.department = department;
+        this.email= email;
+    }
+
+    // ----- Additional Getters & Setters -----
+    public String getDepartment() { return department; }
+    public String getBatch() { return batch; }
+    public String getRoomNumber() { return roomNumber; }
+
+    public void setRoomNumber(String roomNumber) { this.roomNumber = roomNumber; }
+
+    @Override
+    public String toFileString() {
+        return id + "|" + name + "|STUDENT|" + department + "|" + batch + "|" + roomNumber + "|" + passwordHash;
+    }
+
 }
