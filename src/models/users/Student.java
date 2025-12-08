014089a504996cf6a018fa7be8c7bcef1fd182dc
sApphire2004-alt <prochetasilvie.p423@gmail.com>
2025-12-09 00:48:29 +0600

    Modified Some Files

--- src/models/users/Student.java
+++ src/models/users/Student.java
@@ -1,17 +1,42 @@
 package models.users;
 
+import models.complaints.Complaints;
+
 public class Student extends User{
     private String department;
     private String batch;
     private String roomNumber; // initially "UNASSIGNED"
     private String email;
+    Complaints complaints;
 
-    public Student(String id, String name, String role, String passwordHash, String phoneNumber) {
+    public Student(String id, String name, String role, String passwordHash, String phoneNumber, String email, Complaints complaints) {
         super(id, name, role, passwordHash, phoneNumber);
+        this.complaints = complaints;
+        this.email = email;
     }
 
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+    public Complaints getComplaints() {
+        return complaints;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setComplaints(Complaints complaints) {
+        this.complaints = complaints;
     }
 
     // ----- Additional Getters & Setters -----
