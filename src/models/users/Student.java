23cdefa6550ab33bfd904892aff59976b0929fa6
Ayman Binta Altaf Nondiny <ayman@iut-dhaka.edu>
2025-12-25 10:27:04 +0600

    updating student for complaint

--- src/models/users/Student.java
+++ src/models/users/Student.java
@@ -1,5 +1,5 @@
 package models.users;
-
+import libraries.collections.*;
 public class Student extends User{
     private String department;
     private String roomNumber; // initially "UNASSIGNED"
@@ -8,7 +8,7 @@ public class Student extends User{
     public Student(String id, String name, String role, String passwordHash, String phoneNumber) {
         super(id, name, role, passwordHash, phoneNumber);
     }
-
+    
     public void setDepartment(String department) {
         this.department = department;
     }
@@ -23,4 +23,25 @@ public class Student extends User{
     public String toFileString() {
         return id + "|" + name + "|STUDENT|" + department + "|" + passwordHash + "|" + phoneNumber + "|" + email;}
 
+    public String toComplaintFileString()
+    {
+        return id+"|"+name+"|"+roomNumber;
+    }
+
+    public static Student fromFileString(String fileString)
+    {
+        MyString[] parts = new MyString(fileString).split('|');
+        if(parts.length >= 7)
+        {
+            return new Student(
+                parts[0].getValue(),
+                parts[1].getValue(),
+                parts[2].getValue(),
+                parts[4].getValue(),
+                parts[5].getValue()
+            );
+        }
+        return null;
+    } 
+    
 }
