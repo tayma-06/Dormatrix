e004e7143b5cf0ed9c1eed261d39aa90ba0b3532
SayTas <saymatasnim62@gmail.com>
2026-01-27 13:03:31 +0600

    slots class optimization

--- src/libraries/slots/FirstFitAllocator.java
+++ src/libraries/slots/FirstFitAllocator.java
@@ -1,4 +1,14 @@
 package libraries.slots;
 
-public class FirstFitAllocator {
+import exceptions.SlotUnavailableException;
+
+public class FirstFitAllocator extends SlotAllocator {
+    public int findSlot(String[] slots) throws SlotUnavailableException {
+        for (int i = 0; i < slots.length; i++) {
+            if (slots[i] == null) {
+                return i;
+            }
+        }
+        throw new SlotUnavailableException("No available slots found in fridge.");
+    }
 }
