cb91132a0268cd720f3a12ac41c9d3efc42e64c3
sApphire2004-alt <prochetasilvie.p423@gmail.com>
2026-01-27 08:49:42 +0600

    Created Meal Token and Inventory Module

--- src/models/store/Item.java
+++ src/models/store/Item.java
@@ -1,5 +1,36 @@
 package models.store;
 
 public class Item {
-    
+
+    private String itemId;
+    private String name;
+    private int quantity;
+    private double price;
+
+    public Item(String itemId, String name, int quantity, double price) {
+        this.itemId = itemId;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getItemId() { return itemId; }
+    public String getName() { return name; }
+    public int getQuantity() { return quantity; }
+    public double getPrice() { return price; }
+
+    public void reduceQuantity(int amount) {
+        this.quantity -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return itemId + "," + name + "," + quantity + "," + price;
+    }
+
+    public static Item fromString(String line) {
+        String[] p = line.split(",");
+        return new Item(p[0], p[1], Integer.parseInt(p[2]), Double.parseDouble(p[3]));
+    }
 }
+
