fe4e303d5bf21757a7729c038629fdefffb745ae
SayTas <saymatasnim62@gmail.com>
2026-01-27 13:55:01 +0600

    FridgeController update

--- src/controllers/facilities/FridgeController.java
+++ src/controllers/facilities/FridgeController.java
@@ -1,4 +1,20 @@
 package controllers.facilities;
 
+import exceptions.SlotUnavailableException;
+import libraries.slots.FirstFitAllocator;
+
 public class FridgeController {
+    private FirstFitAllocator allocator = new FirstFitAllocator();
+    private String[] currentFridgeSlots = new String[10];
+
+    public void handleFridgeBooking(String studentId) {
+        try {
+            int assignedSlot = allocator.findSlot(currentFridgeSlots);
+            currentFridgeSlots[assignedSlot] = studentId;
+            System.out.println("Allocated slot: " + assignedSlot);
+        } catch (SlotUnavailableException e) {
+            // This is where you handle the exception you created
+            System.err.println("Booking Failed: " + e.getMessage());
+        }
+    }
 }
