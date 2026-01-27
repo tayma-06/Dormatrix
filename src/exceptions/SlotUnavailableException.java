b04051cd10bcabf82262ad60c73ec4051f10b651
SayTas <saymatasnim62@gmail.com>
2026-01-27 13:33:33 +0600

    SlotUnavailableException

--- src/exceptions/SlotUnavailableException.java
+++ src/exceptions/SlotUnavailableException.java
@@ -1,4 +1,16 @@
 package exceptions;
 
-public class SlotUnavailableException {
+/**
+ * Thrown when a user attempts to book a facility slot (Study Room, Fridge, or Laundry)
+ * that is already occupied or otherwise unavailable.
+ */
+public class SlotUnavailableException extends Exception {
+
+    public SlotUnavailableException() {
+        super("The requested slot is currently unavailable.");
+    }
+
+    public SlotUnavailableException(String message) {
+        super(message);
+    }
 }
