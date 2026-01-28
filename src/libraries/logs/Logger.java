7eb51cd9e54d16262ffe2c969d1afb30fe257b32
SayTas <saymatasnim62@gmail.com>
2026-01-28 12:47:18 +0600

    improvement

--- src/libraries/logs/Logger.java
+++ src/libraries/logs/Logger.java
@@ -1,4 +1,23 @@
 package libraries.logs;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Logger {
+    private static final String FILE_PATH = "data/logs.txt";
+    private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+    public static void log(String message) {
+        // BufferedWriter is used for efficient file writing
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
+            String timestamp = dtf.format(LocalDateTime.now());
+            writer.write("[" + timestamp + "] " + message);
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Logger Error: Could not write to logs.txt");
+        }
+    }
 }
